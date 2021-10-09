207f53871eb6b3440d04d60f8750c329a099c29c
IgnacioOre <igorella@alumnos.ubiobio.cl>
2021-10-09 13:23:48 -0300

    Created Spring project

---
+++ src/main/java/cl/ubb/testing/safeit/SafeItApplication.java
@@ -0,0 +1,13 @@
+package cl.ubb.testing.safeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/ubb/testing/safeit/SafeItApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ubb.testing.safeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
