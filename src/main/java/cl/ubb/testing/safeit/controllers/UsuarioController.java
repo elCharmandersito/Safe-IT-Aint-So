1152adb6f753a84da63bb8ae4785ac579361f23c
alejandra.ortiz1701 <alejandra.ortiz1701@alumnos.ubiobio.cl>
2021-10-12 17:21:20 -0300

    Agregue usuarioRepositoryImplementation, y el metodo listar usuarios

--- src/main/java/cl/ubb/testing/safeit/controllers/UsuarioController.java
+++ src/main/java/cl/ubb/testing/safeit/controllers/UsuarioController.java
@@ -1,8 +1,11 @@
 package cl.ubb.testing.safeit.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +30,19 @@ public class UsuarioController {
 			System.out.println(e.getMessage());
 			return new ResponseEntity<Usuario>(HttpStatus.BAD_REQUEST);
 		}
+	}
+	
+	@GetMapping(value = "/listar", produces ="application/json")
+	public ResponseEntity<List<Usuario>> listClientes() {
 		
+		try {
+			List<Usuario> usuarios = usuarioService.getAll();
+			return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new ResponseEntity<List<Usuario>>(HttpStatus.BAD_REQUEST);
+		}
 	}
 	
 
--- src/main/java/cl/ubb/testing/safeit/repositories/UsuarioRepository.java
+++ src/main/java/cl/ubb/testing/safeit/repositories/UsuarioRepository.java
@@ -2,14 +2,20 @@ package cl.ubb.testing.safeit.repositories;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import cl.ubb.testing.safeit.models.Usuario;
 
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+public interface UsuarioRepository{
 
-	List<Usuario> findAll();
+	public void save(Usuario usuario);
+	
+	public List<Usuario> findAll();
+	
+	public Usuario findById(int id);
+	
+	public void deleteById(int id);
+	
+	
 	
 }
